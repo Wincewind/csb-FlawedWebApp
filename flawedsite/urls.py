afd0f528941267f94e70dee61c8d85e57fe62d7a
Wincewind <86107954+Wincewind@users.noreply.github.com>
2023-10-21 16:33:54 +0300

    Added signup page and functionality

--- flawedsite/urls.py
+++ flawedsite/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import index
+from .views import index, signup
 
 urlpatterns = [
     path('', index, name='index'),
 	path('login/', LoginView.as_view(template_name='pages/login.html')),
 	path('logout/', LogoutView.as_view(next_page='/')),
+	path('signup', signup, name='signup'),
     path("admin/", admin.site.urls),
 ]
--- flawedsite/views.py
+++ flawedsite/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from flawedsite.models import Account
 
 
 @login_required
 def index(request):
 	return render(request, 'pages/index.html')
+
+def signup(request:HttpRequest):
+	if request.method == 'POST':
+		new_user = User()
+		new_user.username = request.POST["username"]
+		new_user.email = request.POST["email"]
+		new_user.set_password(request.POST["password"])
+		new_user.save()
+		new_acc = Account()
+		new_acc.user = new_user
+		new_acc.save()
+		return redirect('/')
+	return render(request, 'pages/signup.html')
